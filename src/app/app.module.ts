ed7e30b1b9c3d358f63d918264420ac71c103805
itsglyb <geli.robiso@gmail.com>
2023-10-27 22:49:06 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrdersComponent } from './orders/orders.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsingleComponent,
+    CartComponent,
+    CheckoutComponent,
+    ShopComponent,
+    DashboardComponent,
+    OrdersComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    ProfileDetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlickCarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
